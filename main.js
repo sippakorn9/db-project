e5175b0265785347c9cbbc0e37c6d6722777840c
Arros <titlezakab@windowslive.com>
2018-04-13 23:37:08 +0700

    add login page

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var express = require('express')
 var app = express()
 var ejs =require('ejs')
+var path = require('path')
 var body =require('body-parser').urlencoded({extended:true})
 var mysql = require('mysql')
 
-
-
-
 app.engine('html',ejs.renderFile)
-app.listen(2000)
+app.listen(3000)
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/",(req,res)=>{
     res.render('index.html')
 })
@@ -17,10 +17,15 @@ app.get('/student',(req,res)=>{
     res.render('student.html')
 })
 
+app.get('/teacher',(req,res)=>{
+    res.render('teacher.html')
+})
+
 app.post("/",body,(req,res)=>{
     if(req.body.typeLogin == 'student'){
         res.redirect('/student')
     }
-
-
+    else if(req.body.typeLogin == 'teacher'){
+        res.redirect('/teacher')
+    }
 })
