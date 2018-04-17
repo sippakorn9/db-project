e9858d843df2cc17dc3e907fdefc6013c77166c9
ariya133 <superyong1996@gmail.com>
2018-04-17 21:38:10 +0700

    sql update
    right file

--- sql create table.sql
+++ sql create table.sql
@@ -2,31 +2,49 @@ use sarie;
 
 
 create table student(
-	studentID varchar(20),
-    fName varchar(20),
-    LName varchar(20),
-	bDate date,
+	SID varchar(20),
+    Fname varchar(50),
+    Lname varchar(50),
+	Bdate date,
     sex varchar(1),
-    degree varchar(20),
-    teacherID varchar(20),
+    degree varchar(50),
+    TID varchar(20),
     facultyID varchar(20),
     departmentID varchar(20),
     programID varchar(20),
     loginPassword varchar(256),
-    primary key(studentID));
+    primary key(SID),
+    foreign key(TID)references teacher(TID),
+    foreign key(facultyID,departmentID) references department(facultyID,departmentID));
 
 create table teacher(
-	teacherID varchar(20),
-	fName varchar(20),
-    lName varchar(20),
-	bDate date,
+	TID varchar(20),
+	Fname varchar(20),
+    Lname varchar(20),
+	Bdate date,
     sex varchar(1),
     postion varchar(20),
     facultyID varchar(20),
     departmentID varchar(20),
     loginPassword varchar(256),
-    primary key(teacherID));
+    primary key(TID),
+    foreign key(facultyID,departmentID) references department(facultyID,departmentID));
 
+
+
+create table faculty(
+	facultyID varchar(20),
+	facultyName varchar(20),
+    primary key(facultyID));
+    
+create table department(
+	facultyID varchar(20),
+    departmentID varchar(20),
+	departName varchar(20),
+    primary key(facultyID,departmentID),
+    foreign key(facultyID) references faculty(facultyID));
+
+    
 create table course(
 	courseID varchar(20),
     courseName varchar(20),
@@ -35,20 +53,8 @@ create table course(
 	unit int, 
     facultyID varchar(20),
     departmentID varchar(20),
-    constraint pkCourse primary key(courseID,acdemicYear,term));
-
-create table faculty(
-	facultyID varchar(20),
-	facultyName varchar(20),
-    primary key(facultyID));
-
-create table building(
-	acronym varchar(20),
-    buildingName varchar(20),
-    roomNo varchar(20),
-    roomType varchar(10),
-	seat int,
-    primary key(acronym) );
+    primary key(courseID,academicYear,term),
+    foreign key(facultyID,departmentID) references department(facultyID,departmentID));
     
 create table section(
 	courseID varchar(20),
@@ -58,12 +64,21 @@ create table section(
     seat int,
     acronym varchar(20),
     roomNo varchar(20),
-    learningTime varchar(30),
+    learningTime varchar(100),
 	primary key(courseID,academicYear,term,sectionNo),
-	foreign key(courseID)references course(courseID),
-    foreign key(academicYear)references course(academicYear),
-	foreign key(term)references course(term));
-
+	foreign key(courseID,academicYear,term)references course(courseID,academicYear,term),
+    foreign key(acronym,roomNo) references room(acronym,roomNo));
+    
+    drop table section;
+    
+create table building(
+	acronym varchar(20),
+    buildingName varchar(20),
+    nuberFloor int,
+    facultyID varchar(20),
+    primary key(acronym),
+    foreign key(facultyID) references faculty(facultyID));
+    
 create table room (
 	acronym varchar(20),
     roomNo varchar(20),
@@ -71,10 +86,7 @@ create table room (
     seat int,
     primary key(acronym,roomNO));
     
-create table department(
-	facultyID varchar(20),
-    departmentID varchar(20),
-	departName varchar(20));
+
     
 create table program (
 	facultyID varchar(20),
@@ -90,12 +102,15 @@ create table teaching(
 	term int);
     
 create table register(
-	studentID varchar(20),
+	SID varchar(20),
     courseID varchar(20),
     academicYear year,
 	term int,
     sectionNo varchar(2),
-    grade varchar(1));
+    grade varchar(2) default 'x',
+    primary key (SID,courseID,academicYear,term),
+    foreign key(SID)references student(SID),
+    foreign key(courseID,academicYear,term) references course(courseID,academicYear,term));
     
 create table  testplace(
 	ourseID varchar(20),
@@ -113,3 +128,17 @@ create table contain (
     courseID varchar(20),
     academicYear year,
     term int);
+ 
+ delete from teacher where TID ='SPJ';   
+select * from teacher;
+select * from course;
+delete from faculty where facultyID='ENG';
+insert into faculty value('21','Engineering');
+insert into department value('21','10','Computer');
+insert into teacher value('SPJ','somchai','pasitijude','1954-04-12','M','professer','21','10',sha2('password',256));
+insert into student value('5831080421','ariya','areechot','1996-10-03','M','bechelor','SPJ','21','10','normal',sha2("password",256));
+insert into course value('2110422','DB MGT SYS DESIGN',2017,2,3,21,10);
+insert into building value('ENG3','ENGINEERING 3',4,'21');
+insert into room value('ENG3','204','lecture',40);
+insert into section value('2110422',2017,2,'2',30,'ENG3','204','TU 8.00-9.30,TH 8.00-9.30');
+insert into register value('5831080421','2110422',2017,2,2,'A+');
